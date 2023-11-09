17439e441cc099a61c8361740411a585f2007453
Sarah Elmendorf <sarah.elmendorf@colorado.edu>
2023-11-08 17:55:16 -0700

    initial commit

---
+++ functions/functions.R
@@ -0,0 +1,10 @@
+check_non_numeric <- function(df, var) {
+  row_probs <- which(!is.na(df[[var]]) & is.na(suppressWarnings(as.numeric(df[[var]]))))
+  if (length(row_probs > 0)) {
+    warning(paste("Check file for", var, "\n"))
+    return(df[row_probs, ])
+  } else {
+    cat(paste("all values are numeric for ", var, "\n"))
+    return(NULL)
+  }
+}
