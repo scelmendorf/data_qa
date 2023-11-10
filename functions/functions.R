5b4ccc8fa9585376f907b2a00fdffae5337dfbc5
Sarah Elmendorf <sarah.elmendorf@colorado.edu>
2023-11-09 21:18:34 -0700

    documented check non-numeric and fixed up thru logic tests

--- functions/functions.R
+++ functions/functions.R
@@ -1,3 +1,8 @@
+
+# Function if a variable in a data frame is non-numeric
+# @param df data frame
+# @param var variable to check
+# @return data frame with non-numeric values
 check_non_numeric <- function(df, var) {
   row_probs <- which(!is.na(df[[var]]) & is.na(suppressWarnings(as.numeric(df[[var]]))))
   if (length(row_probs > 0)) {
